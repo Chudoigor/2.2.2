25388a76c02a0e153d1e69199c1b8add38404f9e
Chudoigor <tchudnovsckyigor@yandex.ru>
2024-07-02 22:25:41 +0300

    Feat add new api for cars add amendments to the comments

--- src/main/java/service/CarServiceImpl.java
+++ src/main/java/service/CarServiceImpl.java
@@ -2,9 +2,20 @@ package service;
 
 import objects.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarServiceImpl implements CarService {
+    public static final List<Car> cars = new ArrayList<>(5);
+
+    static {
+        cars.add(new Car("mercedes", 600, 1));
+        cars.add(new Car("BMW", 320, 2));
+        cars.add(new Car("mazda", 626, 3));
+        cars.add(new Car("pegeot", 407, 4));
+        cars.add(new Car("porshe", 911, 5));
+    }
+
 
     @Override
     public List<Car> getCarsList(List<Car> cars, String count) {
--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,27 +1,18 @@
 package web.controller;
 
-import objects.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import service.CarServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+
+import static service.CarServiceImpl.cars;
+
 
 @Controller
-public class carsController {
+public class CarsController {
 
-    private static final List<Car> cars = new ArrayList<>(5);
-
-    static {
-        cars.add(new Car("mercedes", 600, 1));
-        cars.add(new Car("BMW", 320, 2));
-        cars.add(new Car("mazda", 626, 3));
-        cars.add(new Car("pegeot", 407, 4));
-        cars.add(new Car("porshe", 911, 5));
-    }
 
     @GetMapping(value = "/cars")
     public String helloCars(ModelMap model1, HttpServletRequest req) {
